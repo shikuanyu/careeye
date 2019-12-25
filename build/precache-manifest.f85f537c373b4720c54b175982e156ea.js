654d810f42217eda21ba85635b1a5d670ba9f33e
Paul <weiyaopaul@163.com>
2019-12-25 11:37:27 +0800

    add build file

---
+++ build/precache-manifest.f85f537c373b4720c54b175982e156ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ace193f18e3b763abe883c395b361cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "663dc003c5a477287578",
+    "url": "./static/css/2.c37ce0c1.chunk.css"
+  },
+  {
+    "revision": "8f2f910db87b2ce6337f",
+    "url": "./static/css/main.eff9cb29.chunk.css"
+  },
+  {
+    "revision": "663dc003c5a477287578",
+    "url": "./static/js/2.67cf5875.chunk.js"
+  },
+  {
+    "revision": "8f2f910db87b2ce6337f",
+    "url": "./static/js/main.b682b77f.chunk.js"
+  },
+  {
+    "revision": "cc7bae38334b2c6d5427",
+    "url": "./static/js/runtime-main.09b98371.js"
+  }
+]);
